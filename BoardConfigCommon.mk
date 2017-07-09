1d563d4330b034719a4a1f3a2aaeaacfbbbb5a41
Deepak Gangwar <deeepakg1998@gmail.com>
2017-07-09 13:10:40 +0530

    enable common selinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,7 +128,7 @@ ifeq ($(HOST_OS),linux)
 endif
 
 # SELinux
-#-include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
